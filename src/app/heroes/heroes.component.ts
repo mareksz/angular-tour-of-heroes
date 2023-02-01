ec05bd8ed5b5436b1dacaee2b6bf2f55a2ef9b57
mareksz <marek.szaw@gmail.com>
2023-02-01 15:25:28 +0100

    8 files modified, 5 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,12 +7,11 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-// export class HeroesComponent {
-//   hero: Hero = {
-//     id: 1,
-//     name: 'Windstorm',
-//   };
-// }
 export class HeroesComponent {
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
